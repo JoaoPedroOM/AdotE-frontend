d974a2aa6627cc81c92fcad0e7fa52f24b09b6e5
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-02-17 19:50:23 -0300

    feat: Implementada paginação com React Router DOM

---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import App from "../App";
+
+export function AppRoutes() {
+    return (
+      <Routes>
+       <Route path="/" element={
+          <>
+            <App />
+          </>
+        }/>
+      </Routes>
+    );
+  }
