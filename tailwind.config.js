fb2dbd6cb82a529b492b4718269afad83afe6ddd
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-02-17 19:57:10 -0300

    feat: Implementada navbar e adicionada fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+  			main: 'Passion One',
+  			second: 'Englebert',
+  			tertiary: 'Jost'
+  		},
+    },
   },
   plugins: [],
 }
