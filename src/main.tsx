39cb4654f855ac6af8d918e6b9d122742bb93b9f
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-03-04 15:33:01 -0300

    feat: Adicionado analytics vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Routes } from "./routes/index";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
       <Routes />
+      <Analytics />
   </StrictMode>
 );
