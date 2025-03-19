be62a135889142b6c56e570c8a693c53a0b83c6a
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-03-19 16:09:12 -0300

    feat: Alterado formulário de envio de erro para português

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { Analytics } from "@vercel/analytics/react";
 
 Sentry.init({
   dsn: import.meta.env.VITE_SENTRY_DSN,
+
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.browserProfilingIntegration(),
@@ -15,15 +16,24 @@ Sentry.init({
       colorScheme: "dark",
       isNameRequired: true,
       isEmailRequired: true,
+      formTitle: "Relatar Erro",
+      submitButtonLabel: "Enviar Relatório",
+      cancelButtonLabel: "Cancelar",
+      nameLabel: "Nome",
+      emailLabel: "E-mail",
+      messageLabel: "O que aconteceu?",
+      namePlaceholder: "Seu nome",
+      emailPlaceholder: "seu.email@exemplo.com",
+      messagePlaceholder: "Descreva o problema que ocorreu...",
+      successMessageText: "Relatório enviado com sucesso!",
     }),
   ],
   tracesSampleRate: 1.0,
   tracePropagationTargets: ["localhost", /^https:\/\/api-adot-e\.vercel\.app/],
   profilesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1, 
-  replaysOnErrorSampleRate: 1.0 
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
 });
-
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
