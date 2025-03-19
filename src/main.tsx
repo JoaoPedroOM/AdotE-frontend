11a0e1b07763620f0614760b7e41775a3b7892ff
João Pedro <63527881+JoaoPedroOM@users.noreply.github.com>
2025-03-19 13:12:44 -0300

    feat: Implementação do Sentry para monitoramento da aplicação e reporte de erros pelo usuário

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,33 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import "./index.css";
 import { Routes } from "./routes/index";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.browserProfilingIntegration(),
+    Sentry.replayIntegration(),
+    Sentry.feedbackIntegration({
+      colorScheme: "dark",
+      isNameRequired: true,
+      isEmailRequired: true,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/api-adot-e\.vercel\.app/],
+  profilesSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0 
+});
+
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-      <Routes />
-      <Analytics />
+    <Routes />
+    <Analytics />
   </StrictMode>
 );
